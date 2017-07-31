ccbb863f411a76714202ebb11937da125a8522fd
Taras Oleksiv <toleksiv69@gmail.com>
2017-08-01 01:10:21 +0300

    Commit changes

--- src/reverse/Word.java
+++ src/reverse/Word.java
@@ -5,12 +5,37 @@ package reverse;
  */
 public class Word {
     private String string;
+    public static final String TEXT = "abcd!@_ 2n56nnn";
 
     public Word(String string){
         this.string = string;
     }
 
     public static void main(String[] args) {
+        char[] reverse;
+        Word word = new Word("abcd1e");
+        reverse = word.reverseArray(word.getWordToArray());
+        for (int i = 0; i < reverse.length; i++){
+            System.out.print(reverse[i]);
+        }
+        System.out.println();
+        System.out.println(TEXT.replaceAll("[^abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]", ""));
+    }
 
+    private char[] getWordToArray(){
+        char[] chars = string.toCharArray();
+        return chars;
+    }
+
+    private char[] reverseArray(char[] chars){
+        char[] result = new char[chars.length];
+        for (int i = 0; i < chars.length; i++){
+            if (Character.isLetter(chars[i])){
+                result[result.length - 1 - i] = chars[i];
+            } else {
+                result[i] = chars[i];
+            }
+        }
+        return result;
     }
 }
