c4eafda1cfb911bde9048f837802021a7dc59b94
TarasOleksiv <toleksiv69@gmail.com>
2017-08-01 11:51:58 +0300

    Commit task

--- src/reverse/Main.java
+++ src/reverse/Main.java
@@ -1,7 +1,20 @@
 package reverse;
 
+import java.util.Scanner;
+
 /**
  * Created by Taras on 31.07.2017.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введіть слова: ");
+        Scanner sc = new Scanner(System.in);
+        String delims = "[ ]+";
+        String[] words = sc.nextLine().split(delims);
+        System.out.println("Зворотня фраза:");
+        for (int i = 0; i < words.length; i++){
+            Word word = new Word(words[i]);
+            System.out.print(" ");
+        }
+    }
 }
--- src/reverse/Word.java
+++ src/reverse/Word.java
@@ -5,28 +5,64 @@ package reverse;
  */
 public class Word {
     private String string;
-    public static final String TEXT = "abcd!@_ 2n56nnn";
+    private static final String SEARCH_NONTEXT = "[^abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]";     //regex для небуквенних символів
+    private static final String NOTHING = "";                                                               //для виключення небуквенних символів
+    private static final String SEARCH_TEXT = "[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]";     //regex для буквенних символів (латиниця)
+    private static final String GAP = " ";                                                                  //для заміни буквенних символів на пробіли
+    private static final char GAP_CHAR = ' ';                                                               //символьний аналог гепа
 
     public Word(String string){
         this.string = string;
+        showReverse();
     }
 
-    public static void main(String[] args) {
+    private void showReverse() {
         char[] reverse;
-        Word word = new Word("abcd1e");
-        reverse = word.reverseArray(word.getWordToArray());
-        for (int i = 0; i < reverse.length; i++){
-            System.out.print(reverse[i]);
+        char[] nontext;
+        char[] result;
+
+        //ініціюєм масив чарів
+        result = new char[this.string.length()];
+
+        //виділяєм з вхідного стрінга лише буквенну частину і розвертаєм її у зворотню сторону
+        reverse = this.reverseArray(this.getWordToArray(this.getText(this.string)));
+
+        //виділяєм з вхідного стрінга небуквенну частину, залишаючи небуквенні елементи на свої місцях, а буквенні заміняємо пробілами " "
+        nontext = this.getWordToArray(this.getNonText(this.string));
+
+
+        int k = 0;  //лічильник небуквенних символів в небуквенному масиві nontext
+        for (int i = 0; i < result.length; i++){
+            if (nontext[i] != GAP_CHAR){
+                // якщо в небуквенному масиві nontext на i-й позиціх не пробіл, ставим у результуючий масив цей символ з nontext
+                result[i] = nontext[i];
+                k++;
+            } else {
+                // якщо в небуквенному масиві nontext на i-й позиціх пробіл, ставим у результуючий масив символ з буквенного масиву reverse, враховуючи зміщення на k
+                result[i] = reverse[i-k];
+            }
+            System.out.print(result[i]);
         }
-        System.out.println();
-        System.out.println(TEXT.replaceAll("[^abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]", ""));
+
     }
 
-    private char[] getWordToArray(){
+    //конвертуєм стрінг в масив чарів
+    private char[] getWordToArray(String string){
         char[] chars = string.toCharArray();
         return chars;
     }
 
+    //отримуєм стрінг тільки буквенних символів (небуквенні символи видаляєм)
+    private String getText(String string){
+       return string.replaceAll(SEARCH_NONTEXT,NOTHING);
+    }
+
+    //отримуєм стрінг небуквенних символів, в якому всі буквенні символи замінені на пробіл " "
+    private String getNonText(String string){
+        return string.replaceAll(SEARCH_TEXT,GAP);
+    }
+
+    //розвертає масив чарів у зворотньому напрямку
     private char[] reverseArray(char[] chars){
         char[] result = new char[chars.length];
         for (int i = 0; i < chars.length; i++){
